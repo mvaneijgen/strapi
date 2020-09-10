36405ed7bec58b578c78103507bb0a206c21ea13
Pierre Noël <petersg83@gmail.com>
2020-09-10 18:41:21 +0200

    add validators for dz, components and relations
    Signed-off-by: Pierre Noël <petersg83@gmail.com>

--- packages/strapi/lib/services/entity-validator/index.js
+++ packages/strapi/lib/services/entity-validator/index.js
@@ -9,98 +9,98 @@ const _ = require('lodash');
 const { yup, formatYupErrors } = require('strapi-utils');
 const validators = require('./validators');
 
-module.exports = ({ strapi }) => ({
-  /**
-   * Validate some input based on a model schema
-   * @param {Object} model model schema
-   * @param {Object} data input data
-   */
-  async validateEntity(model, data, options = { isDraft: false }) {
-    const validator = createValidator(model, options);
-
-    return validator
-      .validate(data, {
-        abortEarly: false,
-      })
-      .catch(error => {
-        throw strapi.errors.badRequest('ValidationError', { errors: formatYupErrors(error) });
-      });
-  },
-
-  /**
-   * Validate some input for updating based on a model schema
-   * @param {Object} model model schema
-   * @param {Object} data input data
-   */
-  async validateEntityUpdate(model, data, options = { isDraft: false }) {
-    const validator = createUpdateValidator(model, options);
-
-    return validator
-      .validate(data, {
-        abortEarly: false,
-      })
-      .catch(error => {
-        throw strapi.errors.badRequest('ValidationError', { errors: formatYupErrors(error) });
-      });
-  },
-});
-
 const isMedia = attr => {
   return (attr.collection || attr.model) === 'file' && attr.plugin === 'upload';
 };
 
-const createValidator = (model, { isDraft }) => {
-  return yup
+const isSimpleAttribute = attr =>
+  !attr.collection && !attr.model && attr.type !== 'component' && attr.model !== 'dynamiczone';
+
+const createAttributeValidator = createOrUpdate => (attr, data, { isDraft }) => {
+  // simple attribute
+  if (isSimpleAttribute(attr)) {
+    return validators[attr.type](attr, { isDraft });
+  } else {
+    const attributeModels = strapi.db.getModelsByAttribute(attr);
+    if (attributeModels.length === 0) {
+      throw new Error('Validation failed: Model not found');
+    }
+    // component
+    if (attr.type === 'component') {
+      if (_.get(attr, 'repeatable', false) === true) {
+        return yup.array.of(createModelValidator(attributeModels[0], data, { isDraft }));
+      } else {
+        return createModelValidator(createOrUpdate)(attributeModels[0], data, { isDraft });
+      }
+    }
+    // dynamiczone
+    if (attr.type === 'dynamiczone') {
+      return yup
+        .array()
+        .of(
+          yup.object().shape({
+            __component: yup
+              .string()
+              .required()
+              .oneOf(_.keys(strapi.components)),
+          })
+        )
+        .concat(
+          yup.array().of(
+            yup.lazy(item => {
+              const model = strapi.getModel(item.__component);
+              return createModelValidator(model, data, { isDraft });
+            })
+          )
+        );
+    }
+    // relation
+    if (Array.isArray(data)) {
+      yup.array().of(yup.strapiID());
+    } else {
+      return yup.strapiID();
+    }
+  }
+};
+
+const createModelValidator = createOrUpdate => (model, data, { isDraft }) =>
+  yup
     .object(
-      _.mapValues(model.attributes, attr => {
+      _.mapValues(model.attributes, (attr, attrName) => {
         if (isMedia(attr)) {
           return yup.mixed().nullable();
         }
 
-        let validator = createAttributeValidator(attr, { isDraft }).nullable();
+        let validator = createAttributeValidator(createOrUpdate)(attr, data[attrName], {
+          isDraft,
+        }).nullable();
 
-        if (_.has(attr, 'default')) {
+        if (createOrUpdate === 'creation' && _.has(attr, 'default')) {
           validator = validator.default(attr.default);
         }
 
         if (!isDraft && attr.required) {
-          return validator.notNil();
+          if (createOrUpdate === 'creation') {
+            validator = validator.notNil();
+          } else if (createOrUpdate === 'update') {
+            validator = validator.notNull();
+          }
         }
 
         return validator;
       })
     )
     .required();
+
+const createValidateEntity = createOrUpdate => (model, data, { isDraft = false } = {}) => {
+  const validator = createModelValidator(createOrUpdate)(model, data, { isDraft });
+
+  return validator.validate(data, { abortEarly: false }).catch(error => {
+    throw strapi.errors.badRequest('ValidationError', { errors: formatYupErrors(error) });
+  });
 };
 
-const createUpdateValidator = (model, { isDraft }) => {
-  return yup
-    .object(
-      _.mapValues(model.attributes, attr => {
-        if (isMedia(attr)) {
-          return yup.mixed().nullable();
-        }
-
-        const validator = createAttributeValidator(attr, { isDraft }).nullable();
-
-        if (!isDraft && attr.required) {
-          // on edit you can omit a key to leave it unchanged, but if it is required you cannot set it to null
-          return validator.notNull();
-        }
-
-        return validator;
-      })
-    )
-    .required();
-};
-
-/**
- * Validator for existing types
- */
-const createAttributeValidator = (attr, { isDraft }) => {
-  if (_.has(validators, attr.type)) {
-    return validators[attr.type](attr, { isDraft });
-  }
-
-  return yup.mixed();
+module.exports = {
+  validateEntityCreation: createValidateEntity('creation'),
+  validateEntityUpdate: createValidateEntity('update'),
 };
